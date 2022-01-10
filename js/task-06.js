be9ad0520192c8cbae3bff7a4356d207fa0d068d
Denys <kanedius@gmail.com>
2022-01-11 00:28:38 +0200

    js

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", (event) => {
-  if (event.currentTarget.value.length >= inputEl.dataset.length) {
+  if (event.currentTarget.value.length === inputEl.dataset.length) {
     inputEl.classList.remove("invalid");
     inputEl.classList.add("valid");
   } else {
